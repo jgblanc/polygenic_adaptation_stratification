b64bb1d0513e1f6864142999fb006ebd840d92fe
jgblanc <jgblanc@uchicago.edu>
2021-08-19 12:42:00 -0500

    rescale variance

--- code/Simulate_Phenotypes/simulate_phenotypes_4PopSplit_meanshift.R
+++ code/Simulate_Phenotypes/simulate_phenotypes_4PopSplit_meanshift.R
@@ -10,7 +10,9 @@ suppressWarnings(suppressMessages({
 }))
 
 gvalue_file = args[1] #genetic values
+#gvalue_file = 'output/Simulate_Phenotypes/SimpleGrid/E2/C1/h2-0/genos-gwas_common.gvalue.sscore'
 popfile = args[2] # pop file from msprime simulation
+#popfile = "output/Simulate_Genotypes/SimpleGrid/E2/genos.pop"
 output_file = args[3] #name of output file
 h2 = as.numeric(args[4])
 env_s=as.numeric(args[5])
@@ -23,7 +25,9 @@ prs=fread(gvalue_file)
 colnames(prs)<-c("IID","dosage","prs")
 
 sample_size=nrow(prs)
-
+NGWAS = 500000
+N1_gwas = NGWAS / 2
+N2_gwas = NGWAS / 2
 
 #load file containing the pop ID for each inidividual
 pop=fread(popfile,header=F)
@@ -37,9 +41,29 @@ prs=merge(prs, pop, by="IID", sort=F)
 #NOTE: SIGMA_G SET AT h2
 
 # effect on the "first" population (A)
+#pops <- unique(prs$pop)
+#prs$env = rnorm(sample_size,0, sqrt(1 - h2))
+#prs <- prs %>% group_by(pop) %>% mutate(env = env-mean(env))
+#prs <- prs %>% group_by(pop) %>% mutate(env = ifelse(pop == pops[1], env + env_s, env ))
+
+# Calculate average pop phenotype
 pops <- unique(prs$pop)
 prs$env = rnorm(sample_size,0, sqrt(1 - h2))
-prs <- prs %>% group_by(pop) %>% mutate(env = env-mean(env))
+Z1 = prs %>% group_by(pop) %>% summarise(avg = mean(env)) %>% filter(pop == pops[1]) %>% pull(avg)
+Z2 = prs %>% group_by(pop) %>% summarise(avg = mean(env)) %>% filter(pop == pops[2]) %>% pull(avg)
+
+# Rescale averages
+N1 = nrow(prs %>% filter(pop == pops[1]))
+N2 = nrow(prs %>% filter(pop == pops[2]))
+Z1_gwas = sqrt(N1/N1_gwas) * Z1
+Z2_gwas = sqrt(N2/N2_gwas) * Z2
+
+# Recalulate individual phenotypes
+delta1 = Z1 - Z1_gwas
+delta2 = Z2 - Z2_gwas
+prs = prs %>% group_by(pop) %>% mutate(env = ifelse(pop == pops[1], env - delta1, env - delta2))
+
+# Add stratification effect
 prs <- prs %>% group_by(pop) %>% mutate(env = ifelse(pop == pops[1], env + env_s, env ))
 
 #add prs to each of the environmental effects
--- code/Simulate_Phenotypes/simulate_phenotypes_SimpleGrid.R
+++ code/Simulate_Phenotypes/simulate_phenotypes_SimpleGrid.R
@@ -22,6 +22,7 @@ prs=fread(gvalue_file)
 colnames(prs)<-c("IID","dosage","prs")
 
 sample_size=nrow(prs)
+N_GWAS = 500000
 
 #load file containing the pop ID for each inidividual
 pop=fread(popfile,header=F)
@@ -44,13 +45,32 @@ if (env_s == 0) {
 print(shifts)
 
 # Shift environment for individuals in each deme
-prs$env = rnorm(sample_size,0, sqrt(1 - h2))
-prs <- prs %>% group_by(Lat) %>% mutate(env = env-mean(env))
+#prs$env = rnorm(sample_size,0, sqrt(1 - h2))
+#prs <- prs %>% group_by(Lat) %>% mutate(env = env-mean(env))
 #prs <- prs %>% group_by(Pop) %>% mutate(env = env-mean(env))
+#for (i in 1:num_lat_demes) {
+#  prs <- prs %>% group_by(Lat) %>% mutate(env = ifelse(Lat == (i-1), env + shifts[i], env))
+#}
+
+# Calculate average phenotype per pop
+prs$env = rnorm(sample_size,0, sqrt(1 - h2))
+Z <- prs %>% group_by(Pop) %>% summarise(avg = mean(env)) %>% pull(avg)
+
+# Rescale to GWAS size
+n_sim <- prs %>% group_by(Pop) %>% summarise(num = n()) %>% pull(num)
+Z_gwas <- sqrt(n_sim/ ((n_sim/sum(n_sim) * N_GWAS))) * Z
+
+# Rescale individual phenotypes
+delta <- Z - Z_gwas
+pops <- seq(0, length(delta)-1)
+for (i in 1:length(delta)) {
+  prs = prs %>% group_by(Pop) %>% mutate(env = ifelse(Pop == pops[i], env - delta[i], env))
+}
+
+# Add stratification along Latitude
 for (i in 1:num_lat_demes) {
   prs <- prs %>% group_by(Lat) %>% mutate(env = ifelse(Lat == (i-1), env + shifts[i], env))
 }
-
 
 #add prs to each of the environmental effects
 prs = prs %>%
