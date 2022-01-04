eec114f74a02a05d4f925b51651bde53ffc55c28
Jennifer Grace Blanc <jgblanc@midway2-login2.rcc.local>
2022-01-03 18:04:44 -0600

    added phenos

---
+++ code/Make_Plots/concat_phenos_SimpleGrid.R
@@ -0,0 +1,27 @@
+library(dplyr)
+library(data.table)
+
+n <- 100
+reps <- rep(NA, n)
+for (i in 1:n){reps[i] <- paste0("A", i)}
+print(reps)
+h2 <- "h2-0"
+envs <- c("env-0.0", "env-0.5", "env-1.0")
+cases <- c("C1")
+pheno <- c("LAT", "DIAG", "PS")
+dat <- expand.grid(reps, cases, h2, pheno,  envs)
+colnames(dat) <- c("rep", "case", "h2", "pheno", "envs")
+
+agg_all_data <- function(rep, dir_path, case, h2, pheno, envs) {
+  
+  phenos <- fread(paste0(dir_path, rep,"/", case, "/", h2, "/", pheno, "/", envs, "/", "genos-gwas_common.phenos.txt"))
+  colnames(phenos) <- c("#FID", "IID", "pheno_strat")
+  pop <- fread(paste0('../../output/Simulate_Genotypes/SimpleGrid/', rep,"/", "genos.pop"))
+  colnames(pop) <- c("IID", "#FID", "POP", "LAT", "LONG" )
+  
+  df <- inner_join(phenos, pop)%>% select("pheno_strat", POP, LAT, LONG)
+  return(df)
+}
+df <- plyr::mdply(dat, agg_all_data, dir_path = '../../output/Simulate_Phenotypes/SimpleGrid/' )
+
+fwrite(df, "SimpleGrid_phenos_A.txt", row.names=F,quote=F,sep="\t", col.names = T)
