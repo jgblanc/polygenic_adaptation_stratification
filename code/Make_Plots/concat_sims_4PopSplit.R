91101678b0513cea86a026db8339b8bd6da6d2d2
Jennifer Grace Blanc <jgblanc@midway2-login1.rcc.local>
2022-04-02 15:44:42 -0500

    test h2-0.3

--- code/Make_Plots/concat_sims_4PopSplit.R
+++ code/Make_Plots/concat_sims_4PopSplit.R
@@ -6,7 +6,8 @@ reps <- rep(NA, n)
 for (i in 1:n){reps[i] <- paste0("A", i)}
 print(reps)
 h2 <- "h2-0.0"
-envs <- c("env_0.0","env_0.005" ,"env_0.01","env_0.015","env_0.02","env_0.025", "env_0.03", "env_0.035","env_0.04", "env_0.045", "env_0.05", "env_0.055", "env_0.06")
+#envs <- c("env_0.0","env_0.005" ,"env_0.01","env_0.015","env_0.02","env_0.025", "env_0.03", "env_0.035","env_0.04", "env_0.045", "env_0.05", "env_0.055", "env_0.06")
+envs <- c("env_0.0", "env_0.06")
 cases <- c("C1", "C2")
 ts <- c("p-0.50")
 dat <- expand.grid(reps, cases, h2, ts,  envs)
@@ -21,4 +22,4 @@ agg_all_data <- function(rep, dir_path, case, h2, ts, envs) {
 }
 df <- plyr::mdply(dat, agg_all_data, dir_path = '../../output/PGA_test/4PopSplit/' )
 
-fwrite(df, "4PopSplit_h20_A.txt", row.names=F,quote=F,sep="\t", col.names = T)
+fwrite(df, "4PopSplit_h23_A.txt", row.names=F,quote=F,sep="\t", col.names = T)
--- snakefile
+++ snakefile
@@ -5,8 +5,9 @@ CONFIG=["C1", "C2"]
 REP = []
 for i in range(1,101):
   REP.append("A"+str(i))
-HERITABILITY = ["h2-0.0"]
-ENV=["env_0.0","env_0.005" ,"env_0.01","env_0.015","env_0.02","env_0.025", "env_0.03", "env_0.035","env_0.04", "env_0.045", "env_0.05", "env_0.055", "env_0.06"]
+HERITABILITY = ["h2-0.3"]
+#ENV=["env_0.0","env_0.005" ,"env_0.01","env_0.015","env_0.02","env_0.025", "env_0.03", "env_0.035","env_0.04", "env_0.045", "env_0.05", "env_0.055", "env_0.06"]
+ENV = ["env_0.0", "env_0.06"]
 TS=["p-0.50"]
 SIZE=2000
 NUM_RESAMPLE=1000
