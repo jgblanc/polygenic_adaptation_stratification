fc418d7e430e21890907b0e514e89a7b7610d1af
Jennifer Grace Blanc <jgblanc@midway2-login2.rcc.local>
2022-05-02 17:30:26 -0500

    t

--- code/Make_Plots/concat_ts_magnitude.R
+++ code/Make_Plots/concat_ts_magnitude.R
@@ -3,19 +3,19 @@ library(data.table)
 
 n <- 100
 reps <- rep(NA, n)
-for (i in 1:n){reps[i] <- paste0("B", i)}
+for (i in 1:n){reps[i] <- paste0("A", i)}
 print(reps)
 h2 <- "h2-0.3"
-envs <- c("env_0.0", "env_1.0", "env_-1.0")
+envs <- c("env_0.0", "env_1.0", "env_-1.0", "env_0.25", "env_0.5", "env_0.75",  "env_-0.25", "env_-0.5", "env_-0.75")
 cases <- c("C1")
-ts <- c("p-0.54", "p-0.57", "p-0.60","p-0.63","p-0.66")
+ts <- c("p-0.50")
 dat <- expand.grid(reps, cases, h2, ts,  envs)
 colnames(dat) <- c("rep", "case", "h2", "ts", "envs")
 
 agg_all_data <- function(rep, dir_path, case, h2, ts, envs) {
 
-  #output/Simulate_Phenotypes/4PopSplit/{rep}/{config}/{h2}/{ts}/ts_magnitude.txt  
-  Qx <- fread(paste0(dir_path, rep,"/", case, "/", h2, "/", ts, "/",  "ts_magnitude.txt"))
+  #output/Simulate_Phenotypes/4PopSplit/{rep}/{config}/{h2}/{ts}/{env}/ts_magnitude.txt  
+  Qx <- fread(paste0(dir_path, rep,"/", case, "/", h2, "/", ts, "/", envs, "/",  "ts_magnitude.txt"))
   #Qx$type <- c("c", "c.p", "nc", "c-Tm", "c.p-Tm", "nc-Tm", "c-ID", "c.p-ID", "nc-ID", "True")
   
   return(Qx)
