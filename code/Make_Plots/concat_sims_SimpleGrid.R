8ab6f1109f5121a7fea1b51e3d65990c618050b9
Jennifer Grace Blanc <jgblanc@midway2-login1.rcc.local>
2023-02-18 14:15:21 -0600

    t

--- code/Make_Plots/concat_sims_SimpleGrid.R
+++ code/Make_Plots/concat_sims_SimpleGrid.R
@@ -6,7 +6,7 @@ reps <- rep(NA, n)
 for (i in 1:n){reps[i] <- paste0("C", i)}
 print(reps)
 h2 <- "h2-0"
-envs <- c("env-0.0", "env-1.0", "env-0.2")
+envs <- c("env-0.0", "env-1.0", "env-0.2", "env-0.4", "env-0.6", "env-0.8")
 cases <- c("C1")
 test <- c("LAT", "PS")
 pheno <- c("LAT", "DIAG", "PS")
