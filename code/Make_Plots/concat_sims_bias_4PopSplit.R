7dc10b9dd60e4bf6d9f0f3fc9812fb1e141f5b54
Jennifer Grace Blanc <jgblanc@midway2-login1.rcc.local>
2023-02-20 14:50:14 -0600

    t

--- code/Make_Plots/concat_sims_bias_4PopSplit.R
+++ code/Make_Plots/concat_sims_bias_4PopSplit.R
@@ -8,7 +8,7 @@ print(reps)
 h2 <- "joint-0.0"
 envs <- c("env_0.0", "env_0.01", "env_0.02", "env_0.03", "env_0.04", "env_0.05", "env_0.06", "env_0.07", "env_0.08","env_0.09", "env_0.1")
 #envs <- c("env_0.0","env_0.1", "env_0.5", "env_1.0")
-cases <- c("C1")
+cases <- c("C1", "C2")
 ts <- c("p-0.50")
 nc <- c("c-200")
 dat <- expand.grid(reps, cases, h2, ts,  envs, nc)
