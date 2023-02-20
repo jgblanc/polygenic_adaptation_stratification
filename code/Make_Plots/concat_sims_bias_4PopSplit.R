3bb8f190f4650ef97692e36860076e8ce57e0b3e
Jennifer Grace Blanc <jgblanc@midway2-login1.rcc.local>
2023-02-20 11:33:18 -0600

    t

--- code/Make_Plots/concat_sims_bias_4PopSplit.R
+++ code/Make_Plots/concat_sims_bias_4PopSplit.R
@@ -6,8 +6,8 @@ reps <- rep(NA, n)
 for (i in 1:n){reps[i] <- paste0("C", i)}
 print(reps)
 h2 <- "joint-0.0"
-#envs <- c("env_0.0", "env_0.02","env_0.04", "env_0.06","env_0.08", "env_0.1")
-envs <- c("env_0.0","env_0.1", "env_0.5", "env_1.0")
+envs <- c("env_0.0", "env_0.02", "env_0.04", "env_0.04", "env_0.06", "env_0.08", "env_0.1", "env_0.12", "env_0.14","env_1.0")
+#envs <- c("env_0.0","env_0.1", "env_0.5", "env_1.0")
 cases <- c("C1")
 ts <- c("p-0.50")
 nc <- c("c-200")
@@ -23,4 +23,4 @@ agg_all_data <- function(rep, dir_path, case, h2, ts, envs, nc) {
 }
 df <- plyr::mdply(dat, agg_all_data, dir_path = '../../output/PGA_test/4PopSplit/' )
 
-fwrite(df, "C_4PopSplit_joint_bias.txt.gz", row.names=F,quote=F,sep="\t", col.names = T)
+fwrite(df, "C_4PopSplit_joint_bias_Fig1.txt.gz", row.names=F,quote=F,sep="\t", col.names = T)
--- snakefile
+++ snakefile
@@ -6,13 +6,13 @@ REP = []
 for i in range(1,101):
   REP.append("C"+str(i))
 HERITABILITY = ["joint-0.0"]
-#ENV = ["env_0.0"]
-ENV = ["env_0.0","env_0.1", "env_0.5", "env_1.0"]
+ENV = ["env_0.0", "env_0.02", "env_0.04", "env_0.04", "env_0.06", "env_0.08", "env_0.1", "env_0.12", "env_0.14","env_1.0"]
+#ENV = ["env_0.0","env_0.1", "env_0.5", "env_1.0"]
 #TS=["p-0.50", "p-0.53", "p-0.56", "p-0.59", "p-0.62"]
 TS=["p-0.50"]
 #NUM_CAUSAL = ["c-200", "c-2000", "c-20000", "c-all"]
 NUM_CAUSAL = ["c-200"]
-PC=[1,2]
+PC=[1,5]
 SIZE=2000
 NUM_RESAMPLE=1000
 
