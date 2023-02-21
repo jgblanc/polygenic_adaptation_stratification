2ce8667d585f31b078401dc6a6320b789b8d51a6
Jennifer Grace Blanc <jgblanc@midway2-login2.rcc.local>
2023-02-21 09:33:29 -0600

    t

--- code/Make_Plots/concat_sims_bias_4PopSplit.R
+++ code/Make_Plots/concat_sims_bias_4PopSplit.R
@@ -6,10 +6,10 @@ reps <- rep(NA, n)
 for (i in 1:n){reps[i] <- paste0("C", i)}
 print(reps)
 h2 <- "joint-0.0"
-envs <- c("env_0.0", "env_0.01", "env_0.02", "env_0.03", "env_0.04", "env_0.05", "env_0.06", "env_0.07", "env_0.08","env_0.09", "env_0.1")
-#envs <- c("env_0.0","env_0.1", "env_0.5", "env_1.0")
-cases <- c("C1", "C2")
-ts <- c("p-0.50")
+#envs <- c("env_0.0", "env_0.01", "env_0.02", "env_0.03", "env_0.04", "env_0.05", "env_0.06", "env_0.07", "env_0.08","env_0.09", "env_0.1")
+envs <- c("env_0.0","env_0.1", "env_-0.1")
+cases <- c("C1")
+ts <- c("p-0.50", "p-0.53", "p-0.56", "p-0.59", "p-0.62")
 nc <- c("c-200")
 dat <- expand.grid(reps, cases, h2, ts,  envs, nc)
 colnames(dat) <- c("rep", "case", "h2", "ts", "envs", "nc")
@@ -23,4 +23,4 @@ agg_all_data <- function(rep, dir_path, case, h2, ts, envs, nc) {
 }
 df <- plyr::mdply(dat, agg_all_data, dir_path = '../../output/PGA_test/4PopSplit/' )
 
-fwrite(df, "C_4PopSplit_joint_bias_Fig1.txt.gz", row.names=F,quote=F,sep="\t", col.names = T)
+fwrite(df, "C_4PopSplit_joint_bias_TS.txt.gz", row.names=F,quote=F,sep="\t", col.names = T)
