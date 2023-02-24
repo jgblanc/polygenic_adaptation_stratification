b405241a2b379c184048849ad6000a57f15ce85e
jgblanc <jgblanc@uchicago.edu>
2023-02-24 12:32:52 -0600

    t

--- code/Calculate_Tm/SimpleGrid_format_ID_covars.R
+++ code/Calculate_Tm/SimpleGrid_format_ID_covars.R
@@ -14,7 +14,7 @@ if( length(args) != 6){stop("Usage: <pop file> <Tm> <fam file of GWAS> <test typ
 pop_file = args[1]
 Tm_file = args[2]
 fam_file = args[3]
-pheno_type = args[4]
+test_type = args[4]
 output_file = args[5]
 pc_file = args[6]
 
@@ -33,7 +33,7 @@ colnames(vecs)[1] <- "FID"
 pcs <-  vecs[,3:ncol(vecs)]
 
 
-if (pheno_type == "LAT") {
+if (test_type == "LAT") {
 
   # Format datafile
   tmp <- dplyr::inner_join(pops, fam) %>% select(c("#FID", "IID", "LAT"))
@@ -41,7 +41,7 @@ if (pheno_type == "LAT") {
   df <- df[,c(1,2,4,3)]
   colnames(df) <- c("FID","IID", "Tm", "PopID")
 
-} else if (pheno_type == "PS") {
+} else if (test_type == "PS") {
 
   # Format datafile
   tmp <- dplyr::inner_join(pops, fam) %>% select(c("#FID", "IID", "POP"))
@@ -50,7 +50,7 @@ if (pheno_type == "LAT") {
   df <- df %>% mutate(PopID = case_when((POP == 25) ~ 1, (POP != 25) ~ 0)) %>% mutate(PopID = PopID - mean(PopID)) %>% select(c("#FID",	"IID",	"Tm", "PopID"))
   colnames(df) <- c("FID","IID", "Tm", "PopID")
 
-} else if (pheno_type == "DIAG") {
+} else if (test_type == "DIAG") {
 
   id_diag <- c(0,7,14, 21, 28, 35)
   # Format datafile
