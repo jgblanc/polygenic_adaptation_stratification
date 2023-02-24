fcdb4e56784faf88b55cafa2a66aa0d9c35f9c43
jgblanc <jgblanc@uchicago.edu>
2023-02-23 20:48:59 -0600

    t

--- code/Calculate_Tm/format_ID_covars.R
+++ code/Calculate_Tm/format_ID_covars.R
@@ -8,14 +8,14 @@ suppressWarnings(suppressMessages({
 
 args=commandArgs(TRUE)
 
-if( length(args) != 4){stop("Usage: <pop file> <Tm> <fam file of GWAS> <output file> <eigenvec>") }
+if( length(args) != 5){stop("Usage: <pop file> <Tm> <fam file of GWAS> <output file> <eigenvec>") }
 
 # Parse args
 pop_file = args[1]
 Tm_file = args[2]
 fam_file = args[3]
 output_file = args[4]
-#evec_file = args[5]
+evec_file = args[5]
 
 # Read in files
 pops <- fread(pop_file, header = T)
@@ -32,9 +32,9 @@ pops <- unique(df$PopID)
 df <- df %>% mutate(PopID = case_when((PopID == pops[1]) ~ 1, (PopID == pops[2]) ~ 0))
 
 ## Read in eigenvec files
-#vecs <- fread(evec_file)
-#colnames(vecs)[1] <- "FID"
-#df <- inner_join(df, vecs)
+vecs <- fread(evec_file)
+colnames(vecs)[1] <- "FID"
+df <- inner_join(df, vecs)
 df$PC1 <- rnorm(nrow(df))
 print(head(df))
 
