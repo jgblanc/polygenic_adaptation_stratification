94550d319f283c4fa1f5c8081aa0118a14e8b8f0
jgblanc <jgblanc@uchicago.edu>
2023-04-03 16:54:20 -0500

    t

---
+++ code/PGA_test/ts_fig.R
@@ -0,0 +1,120 @@
+# This script takes a setof effect sizes and test vector and computes Q
+
+args=commandArgs(TRUE)
+
+if(length(args)!=3){stop("Rscript calcQ_4PopSplit.R <prefix to effect sizes> <test panel genotypes prefix> <test vector>
+                         <number of times to resample in empirical null> <output prefix> <true effect sizes> <list of PCs>")}
+
+suppressWarnings(suppressMessages({
+  library(data.table)
+  library(dplyr)
+  library(Matrix)
+  library(pgenlibr)
+}))
+
+gwas_prefix = args[1] # causal betas
+test_prefix = args[2] # Prefix to pilnk files
+out_file = args[3]
+
+
+# Function to read in genotype matrix for a set of variants
+read_genos <- function(geno_prefix, betas) {
+
+  pvar <- pgenlibr::NewPvar(paste0(geno_prefix, ".pvar"))
+  d1 <- pgenlibr::NewPgen(paste0(geno_prefix, ".pgen"))
+  var.ids <- betas$ID
+  var.indx <- rep(0, length(var.ids))
+  for (i in 1:length(var.indx)) {
+    var.indx[i] <- pgenlibr::GetVariantsById(pvar,var.ids[i])
+  }
+  X <- ReadList(d1,var.indx, meanimpute=F)
+  colnames(X) <- var.ids
+
+  return(X)
+}
+
+# Read in genotype matrices
+vars <- fread("../output/Simulate_Genotypes/4PopSplit/D1/genos-gwas_common.pvar", skip = 200)
+G <- read_genos("../output/Simulate_Genotypes/4PopSplit/D1/genos-gwas_common", vars)
+X <- read_genos("../output/Simulate_Genotypes/4PopSplit/D1/genos-test_common", vars)
+
+#### Only once #####
+
+# Get allele frequency diff in TP
+pC <- colMeans(X[1:1000,])/2
+pD <- colMeans(X[1001:2000,])/2
+diff <- pD - pC
+
+# Mean center both genotype matrices
+G <- scale(G, scale = F)
+X <- scale(X, scale = F)
+
+# Make 2 pop test vector
+tvec <- scale(c(rep(0,n/2),rep(1,n/2)))
+
+# Compute FGr
+r.all <- t(X) %*% tvec
+Gvar <- apply(G, 2, var)
+FGr <- G %*% (r.all/Gvar)
+FGr <- scale(FGr)
+
+# Set parameters
+prob <- 1
+m <- nrow(G)
+n <- nrow(X)
+L <- ncol(G)
+ncausal_list <- c(1, 2, 10, 100, 500, 1000, 2000)
+
+########################
+
+out <- matrix(NA, nrow = length(ncausal_list), ncol = 5)
+
+for (j in 1:length(ncausal_list)) {
+
+  print(j)
+
+  ncausal <- ncausal_list[j]
+
+  # Generate effect sizes so there is a true signal
+  indx <- sample(seq(1, L), ncausal)
+  diff.causal <- diff[indx]
+  B <- rnorm(ncausal, 0, L/ncausal)
+  for (i in 1:ncausal){
+    if (diff.causal[i] >= 0) {
+      B[i] <- sample(c(-1, 1),1, prob = c((1-prob), prob)) * abs(B[i])
+    } else {
+      B[i] <- sample(c(1, -1),1, prob = c((1-prob), prob)) * abs(B[i])
+    }
+  }
+  betas <- rep(0, nsps)
+  betas[indx] <- B
+
+  # Calculate True GV
+  gwas.gvs <- G[, indx]%*%as.matrix(B)
+  test.gvs <- X[, indx]%*%as.matrix(B)
+
+  # Simulate Phenotypes
+  phenos <- gwas.gvs+rnorm(m)
+
+  # Compute Bhat including Tm
+  Tm_Bhat <- numeric()
+  for(k in 1:nsnps){
+    Tm_Bhat[k] <- lm(phenos~G[,k] + FGr)$coef[2]
+  }
+
+  # Compute different versions of q
+  r.causal <- r.all[indx]
+  true.q <- (1/(n-1)) * (B %*% r.causal)
+  S.q <- (1/(n-1)) * (Tm_Bhat[indx] %*% r.causal)
+  L.q <- (1/(n-1)) * (Tm_Bhat %*% r.all)
+  eq <- true.q * (1 - (ncausal/L))
+
+  tmp <- c(true.q, S.q, L.q, ncausal, eq)
+  out[j,] <- tmp
+}
+
+# Save output
+print(out)
+fwrite(out, out_file,row.names=T,quote=F,sep="\t", col.names = T)
+
+
