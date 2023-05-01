b0217938c8f5f20723379d941dc717dddc484362
jgblanc <jgblanc@uchicago.edu>
2023-05-01 16:35:40 -0500

    t

--- code/PGA_test/calc_Qx_SimpleGrid.R
+++ code/PGA_test/calc_Qx_SimpleGrid.R
@@ -2,8 +2,8 @@
 
 args=commandArgs(TRUE)
 
-if(length(args)!=17){stop("Rscript calc_Tm.R <c.betas> <c.p.betas> <n.c.betas> <num resample> <output prefix>
-                         <true.sscore> <Tvec.txt> <outfile name> <file with number of snps>")}
+if(length(args)!=9){stop("Rscript calc_Qx_SimpleGrid.R <gwas prefix> <tp genotypes prefix> <test vec>
+                         <pop file> <num resample> <output prefix> <true effect sizes> <list of PCs> <outfile PGS>")}
 
 suppressWarnings(suppressMessages({
   library(data.table)
@@ -12,23 +12,18 @@ suppressWarnings(suppressMessages({
   library(pgenlibr)
 }))
 
-c_file = args[1] # causal betas
-cp_file = args[2] # causal p-value betas
-nc_file = args[3] # clumped betas
-c_Tm_file = args[4] # causal betas
-cp_Tm_file = args[5] # causal p-value betas
-nc_Tm_file = args[6] # clumped betas
-c_ID_file = args[7] # causal betas
-cp_ID_file = args[8] # causal p-value betas
-nc_ID_file = args[9] # clumped betas
-geno_prefix = args[10] # Prefix to pilnk files
-lambdaT_file = args[11]
-true_file = args[12] # true PGS
-tvec_file = args[13] # test vect
-pops_file = args[14]
-num = as.numeric(args[15]) # number of times to resapme
-out_pre = args[16] # output prefix
-out_pgs = args[17]
+gwas_prefix = args[1] # causal betas
+geno_prefix = args[2] # Prefix to pilnk files
+tvec_file = args[3] # test vect
+pops_file = args[4]
+num = as.numeric(args[5]) # number of times to resapme
+out_pre = args[6] # output prefix
+true_file = args[7]
+pc_list=args[8]
+pc_list <- as.numeric(strsplit(pc_list,"-")[[1]])
+print(pc_list)
+out_pgs <- args[9]
+
 
 # Function to read in genotype matrix for a set of variants
 read_genos <- function(geno_prefix, betas) {
@@ -53,18 +48,17 @@ calc_Va <- function(geno_mat, es) {
   freq <- colMeans(geno_mat) /2
 
   # Pull out effect sizes only
-  effect_size <- es$BETA
+  effect_size <- es
 
   # Compute Va
-  Va <- 2 * sum((effect_size)^2 * freq * (1 - freq))
+  Va <- 4 * sum((effect_size)^2 * freq * (1 - freq))
   return(Va)
 }
-
 
 # Function to compute PGS
 pgs <- function(X, betas) {
 
-  Bhat_strat <- betas$BETA_Strat
+  Bhat_strat <- betas
   Z_strat <- X %*% Bhat_strat
 
   out <- cbind(Z_strat)
@@ -73,32 +67,14 @@ pgs <- function(X, betas) {
   return(out)
 }
 
-# Function to clean PGS
-stand_PGS <- function(prs, gv_file) {
+# Function to calculate Q
+calc_q <- function(sscore, Va, tvec) {
 
-  # Load True GV
-  gvalue <- fread(gv_file)
-  colnames(gvalue) <- c("#IID", "NAMED_ALLELE_DOSAGE_SUM", "GV")
+  tvec <- as.matrix(tvec)
+  numerator <- (1/(N-1)) * t(tvec) %*% sscore
+  #qhat <- (1/sqrt(N * Va)) * (numerator)
 
-  # Join dataframes by IID
-  df <- as.data.frame(cbind(prs, gvalue$GV))
-  colnames(df) <- c("STRAT", "GV")
-
-  mprs.adj = df%>%
-    mutate(strat.adjusted = STRAT) %>%
-    ungroup() %>% select("strat.adjusted")
-
-  return(mprs.adj)
-}
-
-# Function to calculate Qx
-calc_Qx <- function(mprs, tvec, Va, lambda_T) {
-
-  # Compute Qx Strat
-  Ztest <- t(tvec) %*% mprs$strat.adjusted
-  Qx_strat <- (t(Ztest) %*% Ztest) / (Va*lambda_T)
-
-  return(Qx_strat)
+  return(qhat)
 }
 
 # Function to flip effect sizes
@@ -108,163 +84,250 @@ flip <- function(betas) {
 }
 
 # Function to flip effect sizes and recompute Qx
-en <- function(betas, tvec, Va, X, true_file, lambda_T) {
+en <- function(X, betas, Va, tvec) {
 
   # Flip effect sizes
-  betas$BETA_Strat <- flip(betas$BETA_Strat)
+  betas <- flip(betas)
 
-  # Calculate PGS
+  # Compute PGS
   prs <- pgs(X, betas)
 
-  # Calculate Qx
-  Qx <- t(calc_Qx(stand_PGS(prs, true_file), tvec, Va, lambda_T))
+  ## Calc Qx - Test
+  q <- t(calc_q(prs, Va, tvec))
 
-  return(Qx)
+  return(q)
 }
 
-# Load Lambda_T
-lambda_T <- fread(lambdaT_file)
-lambda_T <- as.numeric(as.character(lambda_T[1,1]))
-
 # Load Test vector
-std.tvec <- fread(tvec_file)
-tvec <- std.tvec$Tvec
+dfTvec <- fread(tvec_file)
+Tvec <- dfTvec$Tvec - mean(dfTvec$Tvec)
+
+# count number of TP individuals
+N <- length(Tvec)
+
+# Rescale test vec to have variance 1
+tvec_scaled <- scale(Tvec)
 
 # Make longitude test vector
 pops <- fread(pops_file)
 fam <-  fread(paste0(geno_prefix, ".psam"))
 colnames(pops) <- c("IID", "FID", "Pop", "Lat", "Long")
 pop <- dplyr::inner_join(pops, fam, by = c("IID"= "IID"))
-Tvec <- pop$Long
-tvec_long <- (Tvec-mean(Tvec))
+Tvec_long <- pop$Long
+tvec_scaled_long <- scale(Tvec_long)
 
-# Make latitude test vector
-pops <- fread(pops_file)
-fam <-  fread(paste0(geno_prefix, ".psam"))
-colnames(pops) <- c("IID", "FID", "Pop", "Lat", "Long")
-pop <- dplyr::inner_join(pops, fam, by = c("IID"= "IID"))
-Tvec <- pop$Lat
-tvec_lat <- (Tvec-mean(Tvec))
 
 # Wrapper function to calculate Qx and empirical p values
-main <- function(beta_file) {
+main <- function(type, snps) {
 
-  # Load effect sizes
-  betas <- fread(beta_file)
+  if(type == "TRUE") {
+    # Load effect sizes
+    beta_file <- true_file
+    betas <- fread(beta_file, header=FALSE)
+    colnames(betas) <- c("ID", "A1", "joint")
+    betas$marginal <- betas$joint
+    #print(head(betas))
+  } else {
+    # Load effect sizes
+    beta_file <- paste0(gwas_prefix, type,".", snps, ".betas.joint")
+    betas <- fread(beta_file, header = TRUE)
+  }
 
   # Load Genotypes
   X <- read_genos(geno_prefix, betas)
 
   # Calculate Va
-  Va <- calc_Va(X, betas)
+  Va_marginal <- calc_Va(X, betas$marginal)
+  Va_joint <- calc_Va(X, betas$joint)
+
+  # Mean center genotypes
+  X <- scale(X, scale = FALSE)
 
   # Calc PGS
-  sscore <- pgs(X, betas)
-  pgs_stan <- stand_PGS(sscore, true_file)
+  sscore_marginal <- pgs(X, betas$marginal)
+  sscore_joint <- pgs(X, betas$joint)
 
-  ## Calc Qx - Test
-  qx <- t(calc_Qx(pgs_stan, tvec, Va, lambda_T))
+  # Calc Q
+  q_marginal <- t(calc_q(sscore_marginal, Va_marginal, tvec_scaled))
+  q_joint <- t(calc_q(sscore_joint, Va_joint, tvec_scaled))
+  q_marginal_long <- t(calc_q(sscore_marginal, Va_marginal,  tvec_scaled_long))
+  q_joint_long <- t(calc_q(sscore_joint, Va_joint, tvec_scaled_long))
 
-  # Generate Empirical null
+  # Generate Empirical null marginal
   redraws <- matrix(0, ncol = 1, nrow = num)
   for (i in 1:num){
-    redraws[i,] <- en(betas, tvec, Va, X, true_file, lambda_T)
+    redraws[i,] <- en(X,  betas$marginal, Va_marginal, tvec_scaled)
+  }
+  print(paste0("Emp var is ", var(redraws)))
+  print(paste0("Excpected var is ", Va_marginal / N))
+
+  # Calculate empirical p-values
+  all_strat <- redraws[,1]
+  p_strat_en_marginal <- length(all_strat[abs(all_strat) > abs(q_marginal[1,1])]) /length(all_strat)
+
+  # Calculate p-value from chi-square marginal
+  p_strat_marginal <- 2 * pnorm(abs(q_marginal[1,1]), mean = 0, sd = sqrt(Va_marginal / N), lower.tail=FALSE)
+
+  # Generate Empirical null joint
+  redraws <- matrix(0, ncol = 1, nrow = num)
+  for (i in 1:num){
+    redraws[i,] <- en(X, betas$joint, Va_joint, tvec_scaled)
+  }
+  print(paste0("Emp var is ", var(redraws)))
+  print(paste0("Excpected var is ", Va_joint / N))
+
+  # Calculate empirical p-values
+  all_strat <- redraws[,1]
+  p_strat_en_joint <- length(all_strat[abs(all_strat) > abs(q_joint[1,1])])/length(all_strat)
+
+  # Calculate p-value from chi-square
+  p_strat_joint <- 2 * pnorm(abs(q_joint[1,1]),mean = 0, sd = sqrt(Va_joint / N), lower.tail=FALSE)
+
+  ##### Longitude
+
+  # Generate Empirical null marginal
+  redraws <- matrix(0, ncol = 1, nrow = num)
+  for (i in 1:num){
+    redraws[i,] <- en(X,  betas$marginal, Va_marginal, tvec_scaled_long)
   }
 
   # Calculate empirical p-values
   all_strat <- redraws[,1]
-  p_strat_en <- length(all_strat[all_strat > qx[1,1]])/length(all_strat)
+  p_strat_en_marginal_long <- length(all_strat[abs(all_strat) > abs(q_marginal_long[1,1])]) /length(all_strat)
 
-  # Calculate p-value from chi-square
-  p_strat <- pchisq(qx[1,1], df=1, lower.tail=FALSE)
+  # Calculate p-value from chi-square marginal
+  p_strat_marginal_long <- 2 * pnorm(abs(q_marginal_long[1,1]),mean = 0, sd = sqrt(Va_marginal / N), lower.tail=FALSE)
 
-
-  ## Calc Qx - Lat
-  qx <- t(calc_Qx(pgs_stan, tvec_lat, Va, lambda_T))
-
-  # Generate Empirical null
+  # Generate Empirical null joint
   redraws <- matrix(0, ncol = 1, nrow = num)
   for (i in 1:num){
-    redraws[i,] <- en(betas, tvec, Va, X, true_file, lambda_T)
+    redraws[i,] <- en(X, betas$joint, Va_joint, tvec_scaled_long)
   }
 
   # Calculate empirical p-values
   all_strat <- redraws[,1]
-  p_strat_en_lat <- length(all_strat[all_strat > qx[1,1]])/length(all_strat)
+  p_strat_en_joint_long <- length(all_strat[abs(all_strat) > abs(q_joint_long[1,1])])/length(all_strat)
 
   # Calculate p-value from chi-square
-  p_strat_lat <- pchisq(qx[1,1], df=1, lower.tail=FALSE)
+  p_strat_joint_long <- 2 * pnorm(abs(q_joint_long[1,1]),mean = 0, sd = sqrt(Va_joint / N), lower.tail=FALSE)
 
-  ## Calc Qx - Long
-  qx <- t(calc_Qx(pgs_stan, tvec_long, Va, lambda_T))
 
-  # Generate Empirical null
-  redraws <- matrix(0, ncol = 1, nrow = num)
-  for (i in 1:num){
-    redraws[i,] <- en(betas, tvec, Va, X, true_file, lambda_T)
-  }
+  # Concatenate output (Qx, p_strat)
+  out <- c(q_marginal, p_strat_marginal , p_strat_en_marginal, q_joint, p_strat_joint, p_strat_en_joint,
+           q_marginal_long, p_strat_marginal_long, p_strat_en_marginal_long, q_joint_long, p_strat_joint_long, p_strat_en_joint_long)
 
-  # Calculate empirical p-values
-  all_strat <- redraws[,1]
-  p_strat_en_long <- length(all_strat[all_strat > qx[1,1]])/length(all_strat)
-
-  # Calculate p-value from chi-square
-  p_strat_long <- pchisq(qx[1,1], df=1, lower.tail=FALSE)
-
-  # Concatenate output (Qx_random, Qx_strat, p_random, p_strat)
-  out <- c(qx, p_strat , p_strat_en, p_strat_lat, p_strat_en_lat, p_strat_long, p_strat_en_long)
   return(out)
 
 }
 
+
 # Run all types of PGS
-out <- matrix(NA, nrow = 9, ncol =7)
-out[1, ] <- main(c_file)
-out[2, ] <- main(cp_file)
-out[3, ] <- main(nc_file)
-out[4, ] <- main(c_Tm_file)
-out[5, ] <- main(cp_Tm_file)
-out[6, ] <- main(nc_Tm_file)
-out[7, ] <- main(c_ID_file)
-out[8, ] <- main(cp_ID_file)
-out[9, ] <- main(nc_ID_file)
-print(out)
+
+### Asecertained
+out_nc <- as.data.frame(matrix(NA, nrow = 4+length(pc_list), ncol =12))
+out_nc[1, ] <- main(type = "TRUE")
+out_nc[2, ] <- main(type = "", snps  = "nc")
+out_nc[3, ] <- main(type = "-Tm", snps = "nc")
+out_nc[4, ] <- main(type = "-ID", snps = "nc")
+for (i in 1:length(pc_list)) {
+  pc <- pc_list[i]
+  out_nc[(4+i), ]  <- main(type = paste0("-",pc), snps = "nc")
+}
+pcs <- paste0("nc-PC", pc_list)
+out_nc$type <- c("true","nc-uncorrected", "nc-Tm", "nc-ID", pcs)
+
+### Causal
+out_c <- as.data.frame(matrix(NA, nrow = 4+length(pc_list), ncol = 12))
+out_c[1, ] <- main(type = "TRUE")
+out_c[2, ] <- main(type = "", snps  = "c")
+out_c[3, ] <- main(type = "-Tm", snps = "c")
+out_c[4, ] <- main(type = "-ID", snps = "c")
+for (i in 1:length(pc_list)) {
+  pc <-  pc_list[i]
+  out_c[(4+i), ]  <- main(type = paste0("-",pc), snps = "c")
+}
+pcs <- paste0("c-PC", pc_list)
+out_c$type <- c("true","c-uncorrected", "c-Tm", "c-ID", pcs)
+
+### Compute  bias
+tq <- out_c[1,1]
+if (is.na(tq)) {
+  tq <- 0
+}
+out <- rbind(out_nc, out_c)
+colnames(out) <- c("q_marginal", "P.Chi_marginal", "P.EN_marginal", "q_joint", "P.Chi_joint", "P.EN_joint",
+                   "q_marginal_long", "P.Chi_marginal_long", "P.EN_marginal_long", "q_joint_long", "P.Chi_joint_long", "P.EN_joint_long", "type")
+out$Bias_marginal <- out$q_marginal - tq
+out$Bias_joint <- out$q_joint - tq
 
 # Save output
-colnames(out) <- c("Qx", "P.Chi", "P.EN", "P.Chi_Lat", "P.EN_Lat", "P.Chi_Long", "P.EN_Long")
-fwrite(out, out_pre,row.names=F,quote=F,sep="\t", col.names = T)
+print(out)
+fwrite(out, out_pre,row.names=T,quote=F,sep="\t", col.names = T)
 
 # Function to just output PGS
-main2 <- function(beta_file) {
+main2 <- function(type, snps) {
 
-  # Load effect sizes
-  betas <- fread(beta_file)
+  if(type == "TRUE") {
+    # Load effect sizes
+    beta_file <- true_file
+    betas <- fread(beta_file, header=FALSE)
+    colnames(betas) <- c("ID", "A1", "joint")
+    betas$marginal <- betas$joint
+    print(head(betas))
+  } else {
+    # Load effect sizes
+    beta_file <- paste0(gwas_prefix, type,".", snps, ".betas.joint")
+    betas <- fread(beta_file, header = TRUE)
+  }
 
   # Load Genotypes
   X <- read_genos(geno_prefix, betas)
 
+  # Calculate Va
+  Va_marginal <- calc_Va(X, betas$marginal)
+  Va_joint <- calc_Va(X, betas$joint)
+
+  # Mean center genotypes
+  X <- scale(X, scale = FALSE)
+
   # Calc PGS
-  sscore <- pgs(X, betas)
+  sscore_marginal <- pgs(X, betas$marginal)
+  sscore_joint <- pgs(X, betas$joint)
 
-  # Calc Qx
-  pgs_stan <- stand_PGS(sscore, true_file)
-
-  return(pgs_stan$strat.adjusted)
+  return(list(sscore_marginal, sscore_joint))
 }
 
 
 # Output File with all the PGS
 fam <- fread(paste0(geno_prefix, ".psam"))
 fam <- fam[,1:2]
-fam$c <- main2(c_file)
-fam$c.p <- main2(cp_file)
-fam$nc <- main2(nc_file)
-fam$c_Tm <- main2(c_Tm_file)
-fam$c.p_Tm <- main2(cp_Tm_file)
-fam$nc_Tm <- main2(nc_Tm_file)
-fam$c_ID <- main2(c_ID_file)
-fam$c.p_ID <- main2(cp_ID_file)
-fam$nc_ID <- main2(nc_ID_file)
-fam$Tvec <- tvec
+
+tmp <- main2(type = "", snps  = "c")
+fam$c.marginal <- tmp[[1]]
+fam$c.joint <- tmp[[2]]
+
+tmp <- main2(type = "", snps  = "nc")
+fam$nc.marginal <- tmp[[1]]
+fam$nc.joint <- tmp[[2]]
+
+tmp <- main2(type = "-Tm", snps  = "c")
+fam$c_Tm.marginal <- tmp[[1]]
+fam$c_Tm.joint <- tmp[[2]]
+
+tmp <- main2(type = "-Tm", snps  = "nc")
+fam$nc_Tm.marginal <- tmp[[1]]
+fam$nc_Tm.joint <- tmp[[2]]
+
+tmp <- main2(type = "-ID", snps  = "c")
+fam$c_ID.marginal <- tmp[[1]]
+fam$c_ID.joint <- tmp[[2]]
+
+tmp <- main2(type = "-ID", snps  = "nc")
+fam$nc_ID.marginal <- tmp[[1]]
+fam$nc_ID.joint <- tmp[[2]]
+
+
+fam$Tvec <- tvec_scaled
+
 
 fwrite(fam, out_pgs,row.names=F,quote=F,sep="\t", col.names = T)
