08b7fc6a516bc895347c8e89cabc2f81664a054f
Jennifer Grace Blanc <jgblanc@midway2-login1.rcc.local>
2023-06-21 15:01:24 -0500

    t

--- code/Plots/concat_pgs_SimpleGrid.R
+++ code/Plots/concat_pgs_SimpleGrid.R
@@ -6,7 +6,7 @@ reps <- rep(NA, n)
 for (i in 1:n){reps[i] <- paste0("A", i)}
 print(reps)
 h2 <- "h2-0"
-envs <- c("env-0.0", "env-1.0", "env-0.2")
+envs <- c("env-1.0", "env-0.2")
 cases <- c("C1")
 test <- c("LAT", "PS")
 pheno <- c("LAT", "DIAG", "PS")
@@ -19,9 +19,9 @@ agg_all_data <- function(rep, dir_path, case, h2, pheno, envs, test) {
   pop <- fread(paste0('../../output/Simulate_Genotypes/SimpleGrid/', rep,"/", "genos.pop"))
   colnames(pop) <- c("IID", "#FID", "POP", "LAT", "LONG" )
   
-  df <- inner_join(PGS, pop) %>% select("nc.joint", "nc_Tm.joint", POP, LAT, LONG)
+  df <- inner_join(PGS, pop) %>% select("nc.joint", "nc_Tm.joint", "nc_10.joint", POP, LAT, LONG)
   return(df)
 }
 df <- plyr::mdply(dat, agg_all_data, dir_path = '../../output/PGA_test/SimpleGrid/' )
 
-fwrite(df, "SimpleGrid_pgs_A.txt.gz", row.names=F,quote=F,sep="\t", col.names = T)
+fwrite(df, "SimpleGrid_pgs_PCs_A.txt.gz", row.names=F,quote=F,sep="\t", col.names = T)
