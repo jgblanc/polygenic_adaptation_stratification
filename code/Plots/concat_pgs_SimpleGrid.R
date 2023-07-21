0bc5ebc8c9ca817a9a2ec06340876e5ad4b474cf
Jennifer Grace Blanc <jgblanc@midway2-login1.rcc.local>
2023-07-21 11:35:40 -0500

    changed confounder

--- code/Plots/concat_pgs_SimpleGrid.R
+++ code/Plots/concat_pgs_SimpleGrid.R
@@ -5,23 +5,25 @@ n <- 100
 reps <- rep(NA, n)
 for (i in 1:n){reps[i] <- paste0("A", i)}
 print(reps)
-h2 <- "h2-0"
-envs <- c("env-1.0", "env-0.2")
+h2 <- "dc-0"
+#envs <- c("env-1.0", "env-0.2")
+envs <- c("env-1.0")
 cases <- c("C1")
-test <- c("LAT", "PS")
-pheno <- c("LAT", "DIAG", "PS")
+test <- c("PS")
+pheno <- c("PS")
 dat <- expand.grid(reps, cases, h2, pheno,  envs, test)
 colnames(dat) <- c("rep", "case", "h2", "pheno", "envs", "test")
 
 agg_all_data <- function(rep, dir_path, case, h2, pheno, envs, test) {
   
-  PGS <- fread(paste0(dir_path, rep,"/", case, "/", h2, "/", pheno, "/", envs, "/", test, "/",  "PGS_ss.txt"))
+  PGS <- fread(paste0(dir_path, rep,"/", case, "/", h2, "/", pheno, "/", envs, "/", test, "/",  "PGS.txt"))
   pop <- fread(paste0('../../output/Simulate_Genotypes/SimpleGrid/', rep,"/", "genos.pop"))
   colnames(pop) <- c("IID", "#FID", "POP", "LAT", "LONG" )
   
-  df <- inner_join(PGS, pop) %>% select("uc", "c", POP, LAT, LONG)
+  df <- inner_join(PGS, pop) %>% select("nc.joint", "nc_Tm.joint", "nc_10.joint", POP, LAT, LONG)
+  #df <- inner_join(PGS, pop) %>% select("uc", "c", POP, LAT, LONG)
   return(df)
 }
 df <- plyr::mdply(dat, agg_all_data, dir_path = '../../output/PGA_test/SimpleGrid/' )
 
-fwrite(df, "SimpleGrid_pgs_ss_A.txt.gz", row.names=F,quote=F,sep="\t", col.names = T)
+fwrite(df, "SimpleGrid_pgs_dc_A.txt.gz", row.names=F,quote=F,sep="\t", col.names = T)
